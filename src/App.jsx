788e942a912e1b05df1b458f0e2995977e18c8ce
carltonwsmith <carltonw.smith@hotmail.com>
2025-10-13 08:26:46 -0700

    Refactor: replace App.js with App.jsx and remove unused App.css; add Todo component

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>My to do list</h1>
+      <div>
+        <h2>Finish fronted simplified</h2>
+        <button>Delete</button>
+        </div>
+      <div>
+        <h2>Finish interview section</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Land $100K+ a Job</h2>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
